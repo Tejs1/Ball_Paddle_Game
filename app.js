499398a9923ed5ac60e53d0e1adbf1858974b48e
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-10-10 23:52:55 +0530

    create a function to group all draw code

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const canvas = document.getElementById("gameCanvas");
 const canvasContext = canvas.getContext("2d");
 
 window.onload = function () {
+  drawEverthing();
+};
+
+//function to group all draw code
+function drawEverthing() {
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
   canvasContext.fillStyle = "white";
   canvasContext.fillRect(399, 0, 1, 600);
   canvasContext.fillStyle = "grey";
   canvasContext.fillRect(200, 100, 400, 400);
-};
+}
