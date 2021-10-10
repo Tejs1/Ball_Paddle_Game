d6068ed81b4c4218acc1f71b65427efc0e1d5ef6
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-10-11 00:01:53 +0530

    basic dynamic animation

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ let ballX = 50;
 let ballY = 50;
 
 window.onload = function () {
-  drawEverthing();
+  setInterval(drawEverthing, 10);
 };
 
 //function to group all draw code
 function drawEverthing() {
+  ballX = ballX + 1;
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
   canvasContext.fillStyle = "white";
   canvasContext.fillRect(399, 0, 1, 600);
   canvasContext.fillStyle = "grey";
-  canvasContext.fillRect(ballX, ballY, 400, 400);
+  canvasContext.fillRect(ballX, ballY, 40, 40);
 }
