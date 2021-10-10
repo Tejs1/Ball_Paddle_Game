7674508c380f8bc2e1890c62fda1357754d76c9f
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-10-10 23:49:24 +0530

    practice with 2d cordinates on canvas

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const canvas = document.getElementById("gameCanvas");
 const canvasContext = canvas.getContext("2d");
 
 window.onload = function () {
-  canvasContext.fillstyle = "black";
+  canvasContext.fillStyle = "black";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  canvasContext.fillStyle = "white";
+  canvasContext.fillRect(399, 0, 1, 600);
+  canvasContext.fillStyle = "grey";
+  canvasContext.fillRect(200, 100, 400, 400);
 };
