ec9867f889fcb8348c6127d5cc60b3c0386c5545
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-10-11 01:07:55 +0530

    bounce ball of from bothe sides

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 const canvas = document.getElementById("gameCanvas");
 const canvasContext = canvas.getContext("2d");
 let ballX = 50;
-let ballY = 50;
+let ballY = 250;
+let ballSpeedX = 5;
+let padddle1X = 10;
+let padddle2X = 780;
+let canvasX = 0;
+let canvasY = 0;
+let paddleWidth = 10;
+let padddle1Y = 200;
+let padddle2Y = 200;
+const space = 10;
 
 window.onload = function () {
   //setting changes fps
   let fps = 60;
-  setInterval(callFunctions, 1000 / fps);
+  setInterval(function () {
+    moveEverthing();
+    drawEverthing();
+  }, 1000 / fps);
 };
 
 // seperating move code from the Draw code
-function callFunctions() {
-  moveEverthing();
-  drawEverthing();
-}
+
 function moveEverthing() {
-  ballX = ballX + 1;
+  ballX = ballX + ballSpeedX;
+  if (ballX >= canvas.width - 2 * paddleWidth - space) {
+    ballSpeedX = -ballSpeedX;
+  }
+  if (ballX <= canvasX + paddleWidth + space) {
+    ballSpeedX = -ballSpeedX;
+  }
 }
 //function to group all draw code
 function drawEverthing() {
   canvasContext.fillStyle = "black";
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  canvasContext.fillRect(canvasX, canvasY, canvas.width, canvas.height);
   //move and resize the box (paddle!)
   canvasContext.fillStyle = "white";
-  canvasContext.fillRect(10, 200, 10, 100);
-  canvasContext.fillStyle = "grey";
-  canvasContext.fillRect(ballX, ballY, 40, 40);
+  canvasContext.fillRect(padddle1X, padddle1Y, paddleWidth, 100);
+  canvasContext.fillStyle = "white";
+  canvasContext.fillRect(padddle2X, padddle2Y, paddleWidth, 100);
+  canvasContext.fillStyle = "red";
+  canvasContext.fillRect(ballX, ballY, 10, 10);
 }
